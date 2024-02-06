e372848abb37015be5c430dc36bbf509d6f1e493
Varun Jain <vjrainuuu@gmail.com>
2024-02-06 05:15:01 -0600

    Add js for current pages

--- mysite.js
+++ mysite.js
@@ -6,4 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("proj").onclick = () => {
         window.location.assign("https://vsjain2004.github.io/projects.html")
     }
+
+    document.getElementById("exp").onclick = () => {
+        window.location.assign("https://vsjain2004.github.io/experience.html")
+    }
+
+    document.getElementById("achieve").onclick = () => {
+        window.location.assign("https://vsjain2004.github.io/achievements.html")
+    }
+
+    document.getElementById("extra").onclick = () => {
+        window.location.assign("https://vsjain2004.github.io/extracurriculars.html")
+    }
+
+    document.getElementById("contact").onclick = () => {
+        window.location.assign("https://vsjain2004.github.io/contact.html")
+    }
 })
