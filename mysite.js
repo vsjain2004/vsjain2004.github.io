b3262d25c64cc53f6c7ed7ce94d6f4827ed5e7e0
Varun Jain <vjrainuuu@gmail.com>
2024-03-09 17:28:51 -0600

    Fix image issues

--- mysite.js
+++ mysite.js
@@ -27,23 +27,23 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.assign("https://vsjain2004.github.io/resume.html")
     }
 
-    if(window.location.href.includes("index")){
-        if(window.innerHeight < window.innerWidth){
-            document.getElementById("self").height = Math.floor(window.innerHeight*0.4);
-        } else {
-            document.getElementById("self").height = Math.floor(window.innerWidth*0.4);
-        }
-    }   
+    // if(window.location.href.includes("index")){
+    //     if(window.innerHeight < window.innerWidth){
+    //         document.getElementById("self").height = Math.floor(window.innerHeight*0.4);
+    //     } else {
+    //         document.getElementById("self").height = Math.floor(window.innerWidth*0.4);
+    //     }
+    // }   
 
-    window.onresize = () => {
-        if(window.location.href.includes("index")){
-            if(window.innerHeight < window.innerWidth){
-                document.getElementById("self").height = Math.floor(window.innerHeight*0.4);
-            } else {
-                document.getElementById("self").height = Math.floor(window.innerWidth*0.4);
-            }
-        }   
-    }
+    // window.onresize = () => {
+    //     if(window.location.href.includes("index")){
+    //         if(window.innerHeight < window.innerWidth){
+    //             document.getElementById("self").height = Math.floor(window.innerHeight*0.4);
+    //         } else {
+    //             document.getElementById("self").height = Math.floor(window.innerWidth*0.4);
+    //         }
+    //     }   
+    // }
 
     if(window.location.href.includes("projects")){
         document.getElementById("One").click();
