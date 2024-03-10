94bffc185cd51361e4a7f3e1b2eb5792a4315379
Varun Jain <vjrainuuu@gmail.com>
2024-03-10 16:19:08 -0500

    Hide empty pages

--- mysite.js
+++ mysite.js
@@ -7,17 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.assign("https://vsjain2004.github.io/projects.html")
     }
 
-    document.getElementById("exp").onclick = () => {
-        window.location.assign("https://vsjain2004.github.io/experience.html")
-    }
+    // document.getElementById("exp").onclick = () => {
+    //     window.location.assign("https://vsjain2004.github.io/experience.html")
+    // }
 
-    document.getElementById("achieve").onclick = () => {
-        window.location.assign("https://vsjain2004.github.io/achievements.html")
-    }
+    // document.getElementById("achieve").onclick = () => {
+    //     window.location.assign("https://vsjain2004.github.io/achievements.html")
+    // }
 
-    document.getElementById("extra").onclick = () => {
-        window.location.assign("https://vsjain2004.github.io/extracurriculars.html")
-    }
+    // document.getElementById("extra").onclick = () => {
+    //     window.location.assign("https://vsjain2004.github.io/extracurriculars.html")
+    // }
 
     document.getElementById("contact").onclick = () => {
         window.location.assign("https://vsjain2004.github.io/contact.html")
